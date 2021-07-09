a752151f6209612b55596050480179727e1eae9f
fernando farandato <fernandofarandato@gmail.com>
2021-07-09 12:30:16 -0300

    Algo Funciona

---
+++ resources/js/contact-script.js
@@ -0,0 +1,46 @@
+﻿/** @format */
+
+const section = window.location.href.split("/");
+console.log(`You are in  ${section[section.length - 1]}`);
+
+$(document).ready(function () {
+    clearInputs();
+
+    $("#formContact").submit(function (e) {
+        e.preventDefault()
+        submitForm();
+    });
+
+    $("#formContact").keydown(function (e) {
+        buttonAvailable();
+    });
+
+    // ! BUG-FIX Close Modal
+    // El modal no se cierra con su boton por defecto por lo que lo hago manualmente
+    $("#closeModal").click(function (e) {
+        $("#submitModal").hide();
+    });
+});
+
+const validateData = function () {
+    return $("#inputEmail").val().trim() && $("#inputMessagge").val().trim() ? true : false;
+};
+
+const buttonAvailable = function () {
+    validateData()
+        ? $("#buttonSubmitForm").removeClass("disabled")
+        : $("#buttonSubmitForm").addClass("disabled")
+};
+
+const submitForm = function (e) {
+    if (validateData()) {
+        $("#submitModal").show();
+        $("#buttonSubmitForm").addClass("disabled")
+        clearInputs();
+    }
+};
+
+const clearInputs = function () {
+    $("#inputEmail").val("")
+    $("#inputMessagge").val("")
+};
---
+++ resources/js/index-script.js
@@ -0,0 +1,3 @@
+﻿/** @format */
+const section = window.location.href.split("/");
+console.log(`You are in  ${section[section.length - 1]}`);
