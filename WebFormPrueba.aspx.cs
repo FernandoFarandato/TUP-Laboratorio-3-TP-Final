00fa6531b7181b62b93ce7ae69f732c79f28b626
fernando farandato <fernandofarandato@gmail.com>
2021-07-08 22:59:09 -0300

    Initial commit

---
+++ WebFormPrueba.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TUP_Laboratorio_3_Tp_Final
+{
+    public partial class WebFormPrueba : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
